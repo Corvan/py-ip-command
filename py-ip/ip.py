a6124a8eb6014709cd6c8b80e2c02cdfbffc8685
Lars Liedtke <lars@familie-liedtke.net>
2019-06-23 15:07:50 +0200

    TASK: move show functionality to method of class Address

--- py-ip/ip.py
+++ py-ip/ip.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import re
 import subprocess
 from typing import Dict
@@ -6,37 +7,47 @@ from typing import Dict
 class IP:
 
     @staticmethod
-    def addr() -> Dict:
-        output = IP.run('addr').stdout.decode()
-        regexes = {"numbers": '(?P<number>[0-9]+: )',
-                   "interfaces": '(?P<interface>[0-9a-zA-Z-@]+: )',
-                   "flags": '(?P<flags><(.*?)>)'}
-
-        all_pattern = re.compile(str().join(regexes.values()))
-
-        interfaces = dict()
-        for find in re.finditer(all_pattern, output):
-            number = int(IP._remove_colon(find.group('number')))
-            name = IP._remove_colon(find.group('interface'))
-            flags = IP._remove_angle_brackets(find.group('flags')).split(",")
-            interfaces[number] = {"name": name,
-                                  "flags": flags}
-
-        return interfaces
+    def addr() -> Address:
+        return Address()
 
     @staticmethod
     def run(command: str) -> subprocess.CompletedProcess:
         ip_path = subprocess.run(['which', 'ip'], capture_output=True).stdout.decode().strip()
         return subprocess.run([ip_path, command], capture_output=True)
 
+
+class Address:
+
+    @staticmethod
+    def show() -> Dict:
+        output = IP.run('addr').stdout.decode()
+        regexes = {"numbers": '(?P<number>[0-9]+: )',
+                   "interfaces": '(?P<interface>[0-9a-zA-Z-@]+: )',
+                   "flags": '(?P<flags><(.*?)>)',
+                   "mtu": '(?: mtu )(?P<mtu>[0-9]+)'}
+
+        all_pattern = re.compile(str().join(regexes.values()))
+
+        interfaces = dict()
+        for find in re.finditer(all_pattern, output):
+            number = int(Address._remove_colon(find.group('number')))
+            name = Address._remove_colon(find.group('interface'))
+            flags = Address._remove_angle_brackets(find.group('flags')).split(",")
+            mtu = find.group('mtu')
+            interfaces[number] = {"name": name,
+                                  "flags": flags,
+                                  "mtu": mtu}
+
+        return interfaces
+
     @staticmethod
     def _remove_colon(text: str) -> str:
-        return IP._remove(":", text)
+        return Address._remove(":", text)
 
     @staticmethod
     def _remove_angle_brackets(text: str) -> str:
-        text = IP._remove("<", text)
-        return IP._remove(">", text)
+        text = Address._remove("<", text)
+        return Address._remove(">", text)
 
     @staticmethod
     def _remove(character: str, text: str) -> str:
@@ -44,4 +55,6 @@ class IP:
 
 
 if __name__ == '__main__':
-    print(IP.addr())
+    print(IP
+          .addr()
+          .show())
