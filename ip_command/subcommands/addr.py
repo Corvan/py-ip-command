bc0ac659d8e382e3c7ea2436900030bf32ac6de1
Lars Liedtke <lars@familie-liedtke.net>
2019-06-30 14:58:18 +0200

    TASK: reorganize classes

--- ip_command/subcommands/addr.py
+++ ip_command/subcommands/addr.py
@@ -8,20 +8,20 @@ from typing import Dict, List, Pattern, Union
 
 
 class Address:
-    regexes = {"number": r'(?P<number>[0-9]+):\s+',
-               "name": r'(?P<name>[0-9a-zA-Z-@]+):\s+',
-               "flags": r'(?:<)(?P<flags>(.*?))(?:>)\s+',
-               "mtu": r'(?:mtu\s)(?P<mtu>[0-9]+)\s+',
-               "qdisc": r'(?:qdisc\s)(?P<qdisc>[a-z]+)\s+',
-               "state": r'(?:state\s)(?P<state>[A-Z]+)\s+',
-               "group": r'(?:group\s)(?P<group>[a-z]+)\s+',
-               "qlen": r'(?:qlen\s)(?P<qlen>[0-9]+)\s+',
-               "link_type": r'(?:link/)(?P<link_type>[a-z]+)\s+',
-               "link_mac_address": r'(?P<link_mac_address>([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}))\s+',
-               "link_mac_broadcast":
-                   r'(?:brd\s)(?P<link_mac_broadcast>([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}))\s{1}',
-               "addresses": r'(?P<addresses>(\s+inet.*\n.*\n)*)'}
-    all_pattern: Pattern = re.compile(str().join(regexes.values()))
+    _regexes = {"number": r'(?P<number>[0-9]+):\s+',
+                "name": r'(?P<name>[0-9a-zA-Z-@]+):\s+',
+                "flags": r'(?:<)(?P<flags>(.*?))(?:>)\s+',
+                "mtu": r'(?:mtu\s)(?P<mtu>[0-9]+)\s+',
+                "qdisc": r'(?:qdisc\s)(?P<qdisc>[a-z]+)\s+',
+                "state": r'(?:state\s)(?P<state>[A-Z]+)\s+',
+                "group": r'(?:group\s)(?P<group>[a-z]+)\s+',
+                "qlen": r'(?:qlen\s)(?P<qlen>[0-9]+)\s+',
+                "link_type": r'(?:link/)(?P<link_type>[a-z]+)\s+',
+                "link_mac_address": r'(?P<link_mac_address>([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}))\s+',
+                "link_mac_broadcast":
+                    r'(?:brd\s)(?P<link_mac_broadcast>([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}))\s{1}',
+                "addresses": r'(?P<addresses>(\s+inet.*\n.*\n)*)'}
+    _all_pattern: Pattern = re.compile(str().join(_regexes.values()))
 
     @staticmethod
     def show(as_dict: bool = False) -> Union[List[Interface], Dict]:
@@ -29,7 +29,7 @@ class Address:
         output = run(['addr', 'show']).stdout.decode()
 
         interfaces: List[Interface] = list()
-        for find in re.finditer(Address.all_pattern, output):
+        for find in re.finditer(Address._all_pattern, output):
             interfaces.append(Interface(number=int(find.group('number')),
                                         name=find.group('name'),
                                         flags=find.group('flags').split(','),
@@ -41,31 +41,27 @@ class Address:
                                         link=Link(type=find.group('link_type'),
                                                   mac_address=find.group('link_mac_address'),
                                                   mac_broadcast=find.group('link_mac_broadcast')),
-                                        addresses=Address._parse_addresses(find.group('addresses'), as_dict)))
+                                        addresses=Address._parse_addresses(find.group('addresses'))))
         if as_dict:
             return {interface.number: dataclasses.asdict(interface) for interface in interfaces}
 
         return interfaces
 
     @staticmethod
-    def _parse_addresses(address_definitions: str, as_dict: bool) -> Union[List[Dict], List[IpAddress]]:
+    def _parse_addresses(address_definitions: str) -> Union[List[Dict],
+                                                                           List[Union[ipaddress.IPv4Interface,
+                                                                                      ipaddress.IPv6Interface]]]:
         regexes = {
             "address_family": r'\s+(?P<address_family>((\binet\b)|(\binet6\b)))\s+',
             "ip_address": r'(?P<ip>(([0-9a-f]{0,4}:+){0,8}([0-9a-f]{0,4})/[0-9]{0,3})|'  # IPv6
-                          r'([0-9]{1,3}\.){3}[0-9]{0,3}/[0-9]{2})\s+'            # IPv4
+                          r'([0-9]{1,3}\.){3}[0-9]{0,3}/[0-9]{2})\s+'                    # IPv4
         }
 
         all_pattern = re.compile(str().join(regexes.values()))
 
         addresses = list()
         for find in re.finditer(all_pattern, address_definitions):
-
-            address = IpAddress(family=find.group('address_family'),
-                                address=ipaddress.ip_interface(find.group('ip')))
-
-            addresses.append(address)
-        if as_dict:
-            return [dataclasses.asdict(address) for address in addresses]
+            addresses.append(ipaddress.ip_interface(find.group('ip')))
         return addresses
 
 
@@ -81,19 +77,14 @@ class Interface:
     group: str
     qlen: str
     link: Link
-    addresses: List[IpAddress]
+    addresses: List[Union[ipaddress.IPv4Interface, ipaddress.IPv6Interface]]
 
 
 @dataclass
 class Link:
-
+    """
+    An :Interface:'s physical link
+    """
     type: str
     mac_address: str
     mac_broadcast: str
-
-
-@dataclass
-class IpAddress:
-
-    family: str
-    address: Union[ipaddress.IPv4Interface, ipaddress.IPv6Interface]
